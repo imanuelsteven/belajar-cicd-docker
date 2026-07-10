db9dbf3f0348a2e4e96b38a1172dc2427f37adb8
imanuelsteven <tugasstevengraciano@gmail.com>
2026-07-10 10:59:00 +0700

    add unittest

---
+++ test-app.py
@@ -0,0 +1,12 @@
+from app import add, greet
+
+
+def test_greet():
+    assert greet("Alice") == "Hello, Alice!"
+    assert greet("Bob") == "Hello, Bob!"
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
